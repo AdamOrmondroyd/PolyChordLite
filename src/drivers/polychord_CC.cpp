4bd4299b6a031f6355237b9191e8c138b8c593fb
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-07-28 18:51:16 +0100

    appropriate bao settings

--- src/drivers/polychord_CC.cpp
+++ src/drivers/polychord_CC.cpp
@@ -4,12 +4,12 @@
 int main()
 {
     int nDims, nDerived;
-    nDims = 3;
-    nDerived = 1;
+    nDims = 2;
+    nDerived = 0;
 
     Settings settings(nDims,nDerived);
 
-    settings.nlive         = 50;
+    settings.nlive         = 200;
     settings.num_repeats   = settings.nDims*5;
     settings.do_clustering = false;
 
