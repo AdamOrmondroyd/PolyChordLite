acb660d783e61b649ee5f64df1812359ef3107ba
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2026-03-02 15:18:40 +0000

    first pass at uv install

--- setup.py
+++ setup.py
@@ -1,141 +1,155 @@
 """
-Python interface to PolyChord
+Custom build script for pypolychord.
 
-Polychord is a tool to solve high dimensional problems.
+This handles the complex build process that requires:
+- Running make to build libchord.so from Fortran/C++ sources
+- Compiling the C++ Python extension
+- Handling MPI/non-MPI builds
 """
 
-from setuptools import setup, Extension, find_packages, Distribution
-from setuptools.command.build_py import build_py as _build_py
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 from distutils.command.clean import clean as _clean
 
-import os, sys, subprocess, shutil
+import os
+import sys
+import subprocess
+import shutil
 
 import numpy
 
+
 def check_compiler(default_CC="gcc"):
     """Checks what compiler is being used (clang, intel, or gcc)."""
-
     CC = default_CC if "CC" not in os.environ else os.environ["CC"]
-    CC_version = subprocess.check_output([CC, "-v"], stderr=subprocess.STDOUT).decode("utf-8").lower()
-    
+    try:
+        CC_version = subprocess.check_output(
+            [CC, "-v"], stderr=subprocess.STDOUT
+        ).decode("utf-8").lower()
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return ""
+
     if "clang" in CC_version:
-        CC_family = "clang"
+        return "clang"
     elif "icc" in CC_version:
-        CC_family = "intel"
+        return "intel"
     elif "gcc" in CC_version:
-        CC_family = "gcc"
+        return "gcc"
     else:
         print("Warning: unrecognised compiler: {}".format(CC_version))
-        CC_family = ""
-
-    return CC_family
+        return ""
 
 
-NAME = 'pypolychord'
-DOCLINES = (__doc__ or '').split("\n")
+def get_compiler_flags():
+    """Get compiler flags based on the detected compiler."""
+    CC_FAMILY = check_compiler()
+    CPPRUNTIMELIB_FLAG = []
+    RPATH_FLAG = []
+
+    if CC_FAMILY == "clang":
+        CPPRUNTIMELIB_FLAG += ["-stdlib=libc++"]
+        if sys.platform == "darwin":
+            CPPRUNTIMELIB_FLAG += ["-mmacosx-version-min=10.9"]
+
+    if sys.platform != "darwin":
+        RPATH_FLAG += ["-Wl,-rpath,$ORIGIN/lib"]
+
+    return CPPRUNTIMELIB_FLAG, RPATH_FLAG
 
 
-# Deal with annoying differences between clang and the other compilers
-CC_FAMILY = check_compiler()
-CPPRUNTIMELIB_FLAG = []
-RPATH_FLAG = []
+class CustomBuildExt(_build_ext):
+    """Custom build_ext that builds libchord.so before building the extension."""
 
-if CC_FAMILY == "clang":
-    CPPRUNTIMELIB_FLAG += ["-stdlib=libc++"]
-    if sys.platform == "darwin":
-        # macOS idiosyncrasies
-        CPPRUNTIMELIB_FLAG += ["-mmacosx-version-min=10.9"]
+    user_options = _build_ext.user_options + [
+        ("no-mpi", None, "Don't compile with MPI support."),
+        ("debug-flags", None, "Compile in debug mode."),
+    ]
 
-if sys.platform != "darwin":
-    # Set RPATH on Linux machines
-    RPATH_FLAG += ["-Wl,-rpath,$ORIGIN/pypolychord/lib"]
-
-
-def readme():
-    with open('pypolychord_README.rst') as f:
-        return f.read()
-
-
-def get_version(short=False):
-    with open('src/polychord/feedback.f90') as f:
-        for line in f:
-            if 'version' in line:
-                return line[44:50]
-
-
-class DistributionWithOption(Distribution, object):
-    def __init__(self, *args, **kwargs):
-        self.global_options = self.global_options \
-                                + [("no-mpi", None, "Don't compile with MPI support."),
-                                   ("debug-flags", None, "Compile in debug mode.")]
+    def initialize_options(self):
+        super().initialize_options()
         self.no_mpi = None
         self.debug_flags = None
-        super(DistributionWithOption, self).__init__(*args, **kwargs)
 
-class CustomBuildPy(_build_py, object):
+    def finalize_options(self):
+        super().finalize_options()
+
     def run(self):
-        env = {}
-        env["PATH"] = os.environ["PATH"]
-        if self.distribution.no_mpi is None:
-            env["MPI"] = "1"
-            # These need to be set so that build_ext uses the right compilers
-            cc_compiler = subprocess.check_output(["make", "print_CC"]).decode('utf-8').strip()
-            os.environ["CC"] = cc_compiler
+        # Build libchord.so using make
+        self._build_libchord()
+        # Then build the Python extension
+        super().run()
 
-            cxx_compiler = subprocess.check_output(["make", "print_CXX"]).decode('utf-8').strip()
-            os.environ["CXX"] = cxx_compiler
-        else:
-            env["MPI"] = "0"
-
-        if self.distribution.debug_flags is not None:
-            self.distribution.ext_modules[0].extra_compile_args += ["-g", "-O0"]
-            env["DEBUG"] = "1"
-        
+    def _build_libchord(self):
+        """Run make to build libchord.so from Fortran/C++ sources."""
         BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+        env = dict(os.environ)
         env["CURDIR"] = BASE_PATH
-        env.update({k : os.environ[k] for k in ["CC", "CXX", "FC"] if k in os.environ})
+
+        if self.no_mpi:
+            env["MPI"] = "0"
+        else:
+            env["MPI"] = "1"
+            # Get MPI compilers from makefile
+            try:
+                cc_compiler = subprocess.check_output(
+                    ["make", "print_CC"], cwd=BASE_PATH
+                ).decode('utf-8').strip()
+                os.environ["CC"] = cc_compiler
+                env["CC"] = cc_compiler
+
+                cxx_compiler = subprocess.check_output(
+                    ["make", "print_CXX"], cwd=BASE_PATH
+                ).decode('utf-8').strip()
+                os.environ["CXX"] = cxx_compiler
+                env["CXX"] = cxx_compiler
+            except (subprocess.CalledProcessError, FileNotFoundError):
+                pass
+
+        if self.debug_flags:
+            env["DEBUG"] = "1"
+            for ext in self.extensions:
+                ext.extra_compile_args += ["-g", "-O0"]
+
+        # Build libchord.so
         subprocess.check_call(["make", "-e", "libchord.so"], env=env, cwd=BASE_PATH)
-        if not os.path.isdir("pypolychord/lib/"):
-            os.makedirs(os.path.join(BASE_PATH, "pypolychord/lib/"))
-        shutil.copy(os.path.join(BASE_PATH, "lib/libchord.so"), 
-                    os.path.join(BASE_PATH, "pypolychord/lib/"))
-        self.run_command("build_ext")
-        return super(CustomBuildPy, self).run()
+
+        # Copy to pypolychord/lib/
+        lib_dest = os.path.join(BASE_PATH, "pypolychord", "lib")
+        os.makedirs(lib_dest, exist_ok=True)
+        shutil.copy(
+            os.path.join(BASE_PATH, "lib", "libchord.so"),
+            lib_dest,
+        )
+
 
 class CustomClean(_clean):
     def run(self):
-        subprocess.run(["make", "veryclean"], check=True, env=os.environ)
+        try:
+            subprocess.run(["make", "veryclean"], check=True, env=os.environ)
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            pass
         return super().run()
 
-if "--no-mpi" in sys.argv:
-    NAME += '_nompi'
-    DOCLINES[1] = DOCLINES[1] + ' (cannot be used with MPI)'
+
+# Get compiler flags
+CPPRUNTIMELIB_FLAG, RPATH_FLAG = get_compiler_flags()
 
 pypolychord_module = Extension(
-        name='_pypolychord',
-        library_dirs=['lib'],
-        include_dirs=['src/polychord', numpy.get_include()],
-        libraries=['chord',],
-        extra_link_args=RPATH_FLAG + CPPRUNTIMELIB_FLAG,
-        extra_compile_args= ["-std=c++11"] + RPATH_FLAG + CPPRUNTIMELIB_FLAG,
-        runtime_library_dirs=['lib'],
-        sources=['pypolychord/_pypolychord.cpp']
-        )
+    name="_pypolychord",
+    library_dirs=["lib", "pypolychord/lib"],
+    include_dirs=["src/polychord", numpy.get_include()],
+    libraries=["chord"],
+    extra_link_args=RPATH_FLAG + CPPRUNTIMELIB_FLAG,
+    extra_compile_args=["-std=c++11"] + RPATH_FLAG + CPPRUNTIMELIB_FLAG,
+    runtime_library_dirs=["lib", "pypolychord/lib"],
+    sources=["pypolychord/_pypolychord.cpp"],
+)
 
-setup(name=NAME,
-      version=get_version(),
-      description='Python interface to PolyChord ' + get_version(),
-      url='https://github.com/PolyChord/PolyChordLite',
-      author='Will Handley',
-      author_email='wh260@cam.ac.uk',
-      license='PolyChord',
-      packages=find_packages(),
-      install_requires=['numpy','scipy'],
-      extras_require={'plotting': 'getdist'},
-      distclass=DistributionWithOption,
-      ext_modules=[pypolychord_module],
-      cmdclass={'build_py' : CustomBuildPy,
-                'clean' : CustomClean},
-      package_data={"" : ["lib/libchord.so"]},
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    ext_modules=[pypolychord_module],
+    cmdclass={
+        "build_ext": CustomBuildExt,
+        "clean": CustomClean,
+    },
+)
